cbb2208141e434714c91d34036986f5ab4eebb90
birch <121175143+birchrust@users.noreply.github.com>
2023-08-05 09:59:49 +0800

    feat: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,15 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
