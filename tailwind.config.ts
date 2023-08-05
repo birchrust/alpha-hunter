9849ee86789c0e38eff4a74897cd5d28ab6b4ade
birch <121175143+birchrust@users.noreply.github.com>
2023-08-05 10:07:23 +0800

    feat: update container‎

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         text: "var(--color-text)",
